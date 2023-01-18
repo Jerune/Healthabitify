c421d7c0f2d814ce807945d56c36776265b865a9
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-01-18 10:35:41 +0100

    Install Firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBd-i0HluEP2GN59uCmOGt5vZRlpLeH80o",
+  authDomain: "healthabitify.firebaseapp.com",
+  projectId: "healthabitify",
+  storageBucket: "healthabitify.appspot.com",
+  messagingSenderId: "160733277781",
+  appId: "1:160733277781:web:19245b393c54a34e8eaedc",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
