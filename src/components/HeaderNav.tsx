b261eba58e2686b881aa3c7f7331fbcb87bf40cd
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-01-26 15:35:00 +0100

    feat: Adjustment Header & Footer
    New styles and elements in both components

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,34 +1,56 @@
-import { useMemo } from 'react'
-import { IconContext } from 'react-icons'
+/* eslint-disable react/jsx-no-bind */
+import { useState } from 'react'
 import { SlLogout, SlSettings } from 'react-icons/sl'
 import { TfiReload } from 'react-icons/tfi'
+import { TiArrowSortedDown, TiArrowSortedUp } from 'react-icons/ti'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo_1b.jpg'
 
 function HeaderNav(): JSX.Element {
-    const iconsStyle = useMemo(
-        () => ({
-            className: 'text-palette-600 h-16',
-        }),
-        []
+    const [sideNavOpen, setSideNavOpen] = useState(true)
+    const sideNavVisibility = sideNavOpen ? 'visible' : 'invisible'
+    const sideNavIcon = sideNavOpen ? (
+        <TiArrowSortedUp />
+    ) : (
+        <TiArrowSortedDown />
     )
 
+    function toggleMenu() {
+        setSideNavOpen((prevState) => !prevState)
+    }
+
     return (
-        <header className="flex flex-row justify-between h-16 w-[90%] mx-[5%] border-b border-solid border-palette-600">
-            <div className="flex flex-row align-middle">
-                <img className="h-full" src={logo} alt="Healthability logo" />
-                <span className="pt-6 logo-text">ealthabitify</span>
-            </div>
-            <section className="flex flex-row gap-4 align-bottom">
-                <IconContext.Provider value={iconsStyle}>
+        <>
+            <header className="flex flex-row justify-between items-center h-16 w-[90%] mx-[5%] border-b border-solid border-palette-600">
+                <div className="flex flex-row items-center">
+                    <img className="h-16" src={logo} alt="Healthability logo" />
+                    <span className="pt-1 logo-text">ealthabitify</span>
+                    <button
+                        type="button"
+                        className="pt-2 ml-3 text-lg"
+                        onClick={toggleMenu}
+                    >
+                        {sideNavIcon}
+                    </button>
+                </div>
+                <section className="flex flex-row gap-4 text-lg">
                     <Link to="/settings">
                         <SlSettings />
                     </Link>
                     <TfiReload />
                     <SlLogout />
-                </IconContext.Provider>
-            </section>
-        </header>
+                </section>
+            </header>
+            <nav
+                className={`flex flex-column fixed top-16 left-0 ${sideNavVisibility}`}
+            >
+                <Link to="/data/vitals" onClick={toggleMenu}>
+                    Vitals
+                </Link>
+                <Link to="/data/vitals">Vitals</Link>
+                <Link to="/data/vitals">Vitals</Link>
+            </nav>
+        </>
     )
 }
 
