316138d3c5c61f89e53c738f31c7aa42b0cfdcdd
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-01-26 11:14:16 +0100

    feat: General Routes setup
    React Router is now setup with all needed templates to show correct content on the correct path

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Connection from './pages/Connection'
+import Dashboard from './pages/Dashboard'
+import DataPage from './pages/DataPage'
+import NotFound from './pages/NotFound'
+import Settings from './pages/Settings'
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<App />} />
+                <Route path="/" element={<Dashboard />} />
+                <Route path="data" element={<DataPage />} />
+                <Route path="settings" element={<Settings />} />
+                <Route path="login" element={<Connection />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     )
