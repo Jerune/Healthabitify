e89b447b59f6b36b95baa46c5210f3ffa096c359
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-01-27 12:51:04 +0100

    feat: Added Transitions + Data Source
    Now data is loaded from separate file + Setting are added with an additional transition effect

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,20 +1,23 @@
 /* eslint-disable react/jsx-no-bind */
 import { useState } from 'react'
-import { RiHeartPulseFill } from 'react-icons/ri'
 import { SlLogout, SlSettings } from 'react-icons/sl'
 import { TfiReload } from 'react-icons/tfi'
-import { TiArrowSortedDown, TiArrowSortedUp } from 'react-icons/ti'
+import {
+    MdOutlineKeyboardArrowRight,
+    MdOutlineKeyboardArrowLeft,
+} from 'react-icons/md'
+import { RiHeartPulseFill } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo_1b.jpg'
 import categoriesData from '../data/categoriesDataMock'
 
 function HeaderNav(): JSX.Element {
-    const [sideNavOpen, setSideNavOpen] = useState(true)
-    const sideNavVisibility = sideNavOpen ? 'visible' : 'invisible'
+    const [sideNavOpen, setSideNavOpen] = useState(false)
+    const sideNavClasses = sideNavOpen ? 'open' : ''
     const sideNavIcon = sideNavOpen ? (
-        <TiArrowSortedUp />
+        <MdOutlineKeyboardArrowLeft />
     ) : (
-        <TiArrowSortedDown />
+        <MdOutlineKeyboardArrowRight />
     )
     const menuCategories = categoriesData.map((category) => {
         return (
@@ -41,7 +44,7 @@ function HeaderNav(): JSX.Element {
                     <span className="pt-1 logo-text">ealthabitify</span>
                     <button
                         type="button"
-                        className="pt-2 ml-3 text-lg"
+                        className="pt-2 ml-3 text-xl"
                         onClick={toggleMenu}
                     >
                         {sideNavIcon}
@@ -56,9 +59,16 @@ function HeaderNav(): JSX.Element {
                 </section>
             </header>
             <nav
-                className={`flex flex-col fixed top-16 left-[5%] ${sideNavVisibility}`}
+                className={`w-auto flex flex-col fixed top-16 left-[5%] pt-3 pr-8 pl-1 pb-8 border-r border-b border-solid border-palette-600 z-50 ${sideNavClasses}`}
             >
                 {menuCategories}
+                <Link
+                    to="/settings"
+                    className="flex flex-row items-center gap-2"
+                >
+                    <SlSettings />
+                    Settings
+                </Link>
             </nav>
         </>
     )
