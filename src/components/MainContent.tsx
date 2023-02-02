5ab1c9822ff3c193053a0297433bddc39716dee8
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-02-02 12:29:45 +0100

    refactor: Added Type to MainContent
    Now children props have type

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,5 +1,7 @@
-function MainContent({ children }) {
-    return <main className="grow w-full py-6 px-9">{children}</main>
+import { ReactNode } from 'react'
+
+function MainContent(children: ReactNode) {
+    return <main className="grow w-full py-6 px-9">{children && children}</main>
 }
 
 export default MainContent
