c4b5f66404c8c7fdf42cab32587b481333238aa4
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-02-08 21:22:36 +0100

    feat: Added links to home page dashboard
    Now logo links to dashboard and menu has dashboard item

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/jsx-no-bind */
 import { SlLogout, SlSettings } from 'react-icons/sl'
+import { MdDashboard } from 'react-icons/md'
 import { TfiReload } from 'react-icons/tfi'
 import { AiOutlineDoubleRight, AiOutlineDoubleLeft } from 'react-icons/ai'
 import { ImLab } from 'react-icons/im'
-import { Link, useNavigate } from 'react-router-dom'
 import * as Icons from 'react-icons/ri'
+import { Link, useNavigate } from 'react-router-dom'
 import { signOut } from 'firebase/auth'
 import { auth } from '../services/firebase'
 import { useAppDispatch, useAppSelector } from '../redux/reduxHooks'
@@ -55,7 +56,13 @@ function HeaderNav(): JSX.Element {
         <>
             <header className="flex flex-row justify-between items-center h-16 w-full bg-palette-600 text-white border-b border-solid border-palette-600">
                 <div className="flex flex-row items-center">
-                    <img className="h-16" src={logo} alt="Healthability logo" />
+                    <Link to="/" onClick={() => dispatch(toggleMenu())}>
+                        <img
+                            className="h-16"
+                            src={logo}
+                            alt="Healthability logo"
+                        />
+                    </Link>
                     <LogoText />
                     {isLoggedIn && (
                         <button
@@ -86,6 +93,16 @@ function HeaderNav(): JSX.Element {
             <nav
                 className={`w-auto h-full flex flex-col fixed top-16 left-0 py-5 pl-8 pr-10 bg-white shadow-lg z-50 ${sideNavClasses}`}
             >
+                <Link
+                    to="/"
+                    className="flex flex-row items-center gap-3 py-2"
+                    onClick={() => dispatch(toggleMenu())}
+                >
+                    <button type="button" className="text-xl">
+                        <MdDashboard />
+                    </button>
+                    <span className="text-lg">Dashboard</span>
+                </Link>
                 <div>{menuCategories}</div>
                 <div className="mt-6">
                     <Link
