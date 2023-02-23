7b15c52190e0399b4a020726c78c93d12350aed2
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-02-23 09:01:34 +0100

    feat: Added metricsMock
    List of all active metrics and their features

--- src/redux/reducers/metricsReducer.ts
+++ src/redux/reducers/metricsReducer.ts
@@ -2,41 +2,13 @@ import { createSlice } from '@reduxjs/toolkit'
 
 // Initial State
 
-const standardMetric = [
-    {
-        id: '',
-        name: '',
-        active: true,
-        onDashboard: false,
-        dataType: 'Amount',
-        unit: '',
-        source: 'Manual',
-        categoryId: '',
-        categoryIcon: '',
-        isFixed: true,
-        frequency: 'Weekly',
-        goal: '',
-        conditionsMode: 'Higher',
-        good: {
-            mode: 'More',
-            value: '',
-        },
-        medium: {
-            value1: '',
-            value2: '',
-        },
-        bad: {
-            mode: 'Less',
-            value: '',
-        },
-    },
-]
+const defaultState = []
 
 // Reducer
 
 export const metricSlice = createSlice({
     name: 'metric',
-    initialState: standardMetric,
+    initialState: defaultState,
     reducers: {
         initMetrics: (state, action) => {
             return action.payload
