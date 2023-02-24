1c7c9b66ec7a5676fa3894edc5fa3718a52c003f
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-02-24 12:27:18 +0100

    feat: Change weekly time view
    Can now switch weeks by clicking on the arrows. Correct time will be shown

--- src/components/TimeSelectionModule.tsx
+++ src/components/TimeSelectionModule.tsx
@@ -1,10 +1,17 @@
 import { DateTime } from 'luxon'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai'
 import type { TabListProps } from '../types'
 
 function TimeSelectionModule({ tabs }: TabListProps) {
     const [activeTab, setActiveTab] = useState(0)
+    const [currentTimeData, setCurrentTimeData] = useState({
+        currentDate: DateTime.now(),
+        year: 0,
+        weekNumber: 0,
+        firstDayOfTheWeek: DateTime.now(),
+        lastDayOfTheWeek: DateTime.now(),
+    })
     const listOfTabs = tabs.map((tab, index) => {
         const tabClasses =
             index === activeTab
@@ -26,40 +33,71 @@ function TimeSelectionModule({ tabs }: TabListProps) {
         )
     })
 
-    function getCorrectWeekDates(date: DateTime) {
-        let lastDayOfTheWeek = date
-        const { weekNumber, year } = date
+    function getCorrectWeekDates(): void {
+        let lastDayOfTheWeek = currentTimeData.currentDate
+        const { weekNumber, year } = currentTimeData.currentDate
         for (let i = 1; i < 7; i += 1) {
-            const newDate = date.plus({ days: i })
+            const newDate = currentTimeData.currentDate.plus({ days: i })
             if (newDate.weekNumber === weekNumber) {
                 lastDayOfTheWeek = newDate
             }
         }
         const firstDayOfTheWeek = lastDayOfTheWeek.minus({ days: 7 })
 
-        return { weekNumber, year, firstDayOfTheWeek, lastDayOfTheWeek }
+        setCurrentTimeData((prevState) => {
+            return {
+                ...prevState,
+                weekNumber,
+                year,
+                firstDayOfTheWeek,
+                lastDayOfTheWeek,
+            }
+        })
     }
 
-    const { weekNumber, year, firstDayOfTheWeek, lastDayOfTheWeek } =
-        getCorrectWeekDates(DateTime.now())
-    const timeLeft = firstDayOfTheWeek.toFormat('LLL d')
-    const timeRight = lastDayOfTheWeek.toFormat('LLL d')
+    useEffect(() => {
+        getCorrectWeekDates()
+    }, [currentTimeData.currentDate])
+
+    function changeTimeView(direction: string) {
+        if (direction === 'previous') {
+            setCurrentTimeData((prevState) => {
+                return {
+                    ...prevState,
+                    currentDate: prevState.currentDate.minus({ days: 7 }),
+                }
+            })
+        } else if (direction === 'next') {
+            setCurrentTimeData((prevState) => {
+                return {
+                    ...prevState,
+                    currentDate: prevState.currentDate.plus({ days: 7 }),
+                }
+            })
+        }
+    }
 
     return (
         <div className="w-full flex flex-col items-center p-6 rounded-lg bg-gray-50">
             <ul className="flex items-center gap-3">{listOfTabs}</ul>
             <span className="flex justify-center w-content pt-3 italic">
-                {`Week ${weekNumber}, ${year}`}
+                {`Week ${currentTimeData.weekNumber}, ${currentTimeData.year}`}
             </span>
             <div className="flex flex-row items-center pt-2">
-                <button type="button">
+                <button
+                    type="button"
+                    onClick={() => changeTimeView('previous')}
+                >
                     <AiOutlineLeft />
                 </button>
                 <span className="flex justify-center gap-6 px-8 italic">
-                    {timeLeft} <span>-</span> {timeRight}
+                    {currentTimeData.firstDayOfTheWeek.toFormat('LLL d')}{' '}
+                    <span>-</span>{' '}
+                    {currentTimeData.lastDayOfTheWeek.toFormat('LLL d')}
                 </span>
-                <button type="button">
-                    <AiOutlineRight />
+                <button type="button" onClick={() => changeTimeView('next')}>
+                    {currentTimeData.currentDate.weekNumber <
+                        DateTime.now().weekNumber && <AiOutlineRight />}
                 </button>
             </div>
         </div>
