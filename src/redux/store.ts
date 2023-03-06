0a8a89bf287f1b4b501ac55b8f43eb2075e4b429
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-06 11:56:05 +0100

    feat: Removed API usage Redux
    OuraAPI is no longer directly handled via Redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,17 +3,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import usersReducer from './reducers/usersReducer'
 import utilsReducer from './reducers/utilsReducer'
 import metricsReducer from './reducers/metricsReducer'
-import { useOuraAPI } from '../hooks/useOuraAPI'
 
 const store = configureStore({
     reducer: {
         users: usersReducer,
         metrics: metricsReducer,
         utils: utilsReducer,
-        [useOuraAPI.reducerPath]: useOuraAPI.reducer,
     },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(useOuraAPI.middleware),
 })
 
 store.subscribe(() => console.log(store.getState()))
