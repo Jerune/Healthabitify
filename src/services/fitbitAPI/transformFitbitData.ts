0aad46e9f3545345fc625c119f89fbf3a5927d48
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-07 16:22:52 +0100

    feat: transformFitbitData function
    transforms the data coming from the fitbit api and transforms these to format fitting firebase

--- src/services/fitbitAPI/transformFitbitData.ts
+++ src/services/fitbitAPI/transformFitbitData.ts
@@ -1,10 +1,33 @@
 import addDatapoints from '../../firebase/firestore/data-points/addDatapoints'
-import { FitbitRawData } from '../../types'
+import { DataPoint, FitbitData, FitbitRawData } from '../../types'
+import getDateTimeDataForDatapoints from '../../utils/getDateTimeData'
+import matchServiceResourcesWithMetricNames from '../matchResources'
 
-export default function transformFitbitData(data: FitbitRawData) {
-    const resource = Object.keys(data)[0]
+export default function transformFitbitData(fitbitData: FitbitRawData): void {
+    const source = 'fitbit'
+    const resourceNameFromAPI = Object.keys(fitbitData)[0]
+    const metric = matchServiceResourcesWithMetricNames(
+        source,
+        resourceNameFromAPI
+    )
 
-    if (resource === 'activities-activityCalories') {
-        addDatapoints(data[resource])
-    }
+    const newDataPoints: DataPoint[] = fitbitData[resourceNameFromAPI].map(
+        (datapoint: FitbitData) => {
+            const date = datapoint.dateTime
+            const { value } = datapoint
+            const { month, weekNumber, year } =
+                getDateTimeDataForDatapoints(date)
+            return {
+                userId: 'nbkxUOC66VVE7CbqhloaTQJKiRH3',
+                value,
+                date,
+                source,
+                metric,
+                weekNumber,
+                month,
+                year,
+            }
+        }
+    )
+    addDatapoints(newDataPoints)
 }
