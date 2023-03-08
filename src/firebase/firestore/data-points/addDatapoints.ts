b866a1aba756461a0ac13c875e575777a98d3e29
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-08 15:37:06 +0100

    feat: AddDatapoints refreshes lastUpdated value
    Now lastUpdated value of certain service API is updated after all new datapoints have been adde to the db

--- src/firebase/firestore/data-points/addDatapoints.ts
+++ src/firebase/firestore/data-points/addDatapoints.ts
@@ -3,14 +3,19 @@ import { collection, addDoc } from 'firebase/firestore'
 import type { DataPoint } from '../../../types'
 import { db } from '../../firebase'
 import updateWearables from '../wearables/updateWearables'
+import { getTodaysDateAsString } from '../../../utils/getDatesAsString'
 
 async function addDatapoints(datapoints: DataPoint[]) {
+    const today = getTodaysDateAsString()
     datapoints.forEach(async (datapoint) => {
         try {
             await addDoc(collection(db, 'data-points'), { ...datapoint })
         } catch (e) {
             console.error('Error adding document: ', e)
         }
+    })
+    updateWearables('fitbit', {
+        lastUpdated: today,
     })
 }
 
