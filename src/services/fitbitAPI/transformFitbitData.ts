af725fc9c6e32edb9dd9722aa4b7ada3fb12b2c0
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-08 15:35:55 +0100

    feat: transformFitbitData now returns data
    function now returns a prepared list of datapoints instead of calling a function

--- src/services/fitbitAPI/transformFitbitData.ts
+++ src/services/fitbitAPI/transformFitbitData.ts
@@ -1,22 +1,19 @@
-import { DateTime } from 'luxon'
-import addDatapoints from '../../firebase/firestore/data-points/addDatapoints'
-import updateWearables from '../../firebase/firestore/wearables/updateWearables'
 import { DataPoint, FitbitData, FitbitRawData } from '../../types'
 import getDateTimeDataForDatapoints from '../../utils/getDateTimeData'
-import getTodaysDateAsString from '../../utils/getTodaysDateAsString'
 import matchServiceResourcesWithMetricNames from '../matchResources'
 
-export default function transformFitbitData(fitbitData: FitbitRawData): void {
-    const today = getTodaysDateAsString()
+export default function transformFitbitData(fitbitData: FitbitRawData[]) {
     const source = 'fitbit'
-    const resourceNameFromAPI = Object.keys(fitbitData)[0]
-    const metric = matchServiceResourcesWithMetricNames(
-        source,
-        resourceNameFromAPI
-    )
+    const datapointsToAdd = fitbitData.map((datapointsCollection) => {
+        const resourceNameFromAPI = Object.keys(datapointsCollection)[0]
+        const metric = matchServiceResourcesWithMetricNames(
+            source,
+            resourceNameFromAPI
+        )
 
-    const newDataPoints: DataPoint[] = fitbitData[resourceNameFromAPI].map(
-        (datapoint: FitbitData) => {
+        const newDataPoints: DataPoint[] = datapointsCollection[
+            resourceNameFromAPI
+        ].map((datapoint: FitbitData) => {
             const date = datapoint.dateTime
             const { value } = datapoint
             const { month, weekNumber, year } =
@@ -31,10 +28,9 @@ export default function transformFitbitData(fitbitData: FitbitRawData): void {
                 month,
                 year,
             }
-        }
-    )
-    addDatapoints(newDataPoints)
-    updateWearables('fitbit', {
-        lastUpdated: today,
+        })
+        return newDataPoints
     })
+
+    return datapointsToAdd
 }
