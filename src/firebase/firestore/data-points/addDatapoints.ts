bbcfc5a15b6daad77c8f7315d1a1b26ec401895e
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-09 09:47:24 +0100

    feat: Updates Wearables lastUpdated date differently
    lastUpdated date is now updated dependent on the final date of the data that is received through a service API, so when no data is received lastUpdated date will be less that today

--- src/firebase/firestore/data-points/addDatapoints.ts
+++ src/firebase/firestore/data-points/addDatapoints.ts
@@ -1,12 +1,19 @@
 /* eslint-disable no-console */
 import { collection, addDoc } from 'firebase/firestore'
+import { DateTime } from 'luxon'
 import type { DataPoint } from '../../../types'
 import { db } from '../../firebase'
 import updateWearables from '../wearables/updateWearables'
-import { getTodaysDateAsString } from '../../../utils/getDatesAsString'
+import { getSpecifiedDateAsString } from '../../../utils/getDatesAsString'
 
 async function addDatapoints(datapoints: DataPoint[]) {
-    const today = getTodaysDateAsString()
+    let highestDate = null
+    for (let i = 0; i < datapoints.length; i += 1) {
+        const currentDate = new Date(datapoints[i].date)
+        if (highestDate === null || currentDate > highestDate) {
+            highestDate = currentDate
+        }
+    }
     datapoints.forEach(async (datapoint) => {
         try {
             await addDoc(collection(db, 'data-points'), { ...datapoint })
@@ -14,9 +21,18 @@ async function addDatapoints(datapoints: DataPoint[]) {
             console.error('Error adding document: ', e)
         }
     })
-    updateWearables('fitbit', {
-        lastUpdated: today,
-    })
+
+    if (highestDate) {
+        const highestDateAsDateTime = DateTime.fromJSDate(highestDate)
+        // Sets lastUpdated value to datapoint with highest date + 1
+        // this is to avoid duplicate data on when retrieving data next time
+        const newLastUpdated = highestDateAsDateTime.plus({ days: 1 })
+        const newLastUpdatedAsString = getSpecifiedDateAsString(newLastUpdated)
+
+        updateWearables('fitbit', {
+            lastUpdated: newLastUpdatedAsString,
+        })
+    }
 }
 
 export default addDatapoints
