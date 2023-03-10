6d23383beb646943b3a5ec5812b6cf7806dd5057
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-10 11:44:28 +0100

    feat: ProtectedRoute component to protect authenticated routes

---
+++ src/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from 'react-router-dom'
+import type { PropsWithChildren, ReactNode } from 'react'
+import { useAppSelector } from './redux/reduxHooks'
+
+export default function ProtectedRoute({ children }: PropsWithChildren) {
+    const isLoggedIn = useAppSelector((state) => state.user.isLoggedIn)
+
+    if (!isLoggedIn) {
+        return <Navigate to="/" replace />
+    }
+
+    return children || <Outlet />
+}
