2c46bf34802c840370231ff04fd9dc21cc7a44e7
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-14 12:16:08 +0100

    feat: Added Types for DateTimeData and titles

--- src/features/TimesDatesModule/TimesDatesTypes.d.ts
+++ src/features/TimesDatesModule/TimesDatesTypes.d.ts
@@ -1,6 +1,23 @@
+import { DateTime } from 'luxon'
+
 export type TabListProps = {
     tabs?: {
         name: string
         function: () => void
     }[]
+}
+
+export type DateTimeData = {
+    currentDate: DateTime
+    weekNumber: number
+    month: number
+    year: number
+    firstDayOfTheWeek: DateTime
+    lastDayOfTheWeek: DateTime
+}
+
+export type DataTitles = {
+    week: string
+    month: string
+    year: string
 }
