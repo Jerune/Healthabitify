2883511c5d93763ae536d13f799147e5ef735297
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-14 09:49:04 +0100

    feat: Move also DateTime values to Global state
    Now weekNumber, year, currentDate and month are also available globally for dashboard as well as TimeSlecetionModule as well as datapages to be used

--- src/redux/reducers/utilsReducer.ts
+++ src/redux/reducers/utilsReducer.ts
@@ -1,10 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { DateTime } from 'luxon'
+import { getSpecifiedDateAsString } from '../../utils/getDatesAsString'
+
+// setting initial time values
+const todayOneWeekAgo = DateTime.now().minus({ days: 7 })
+const currentDateTime = {
+    currentDate: getSpecifiedDateAsString(todayOneWeekAgo),
+    year: todayOneWeekAgo.year,
+    month: todayOneWeekAgo.month,
+    weekNumber: todayOneWeekAgo.weekNumber,
+    firstDayOfTheWeek: getSpecifiedDateAsString(
+        todayOneWeekAgo.minus({ days: 7 })
+    ),
+    lastDayOfTheWeek: getSpecifiedDateAsString(
+        todayOneWeekAgo.plus({ days: 7 })
+    ),
+}
 
 // Initial State
-
 const utils = {
     sideNavOpen: false,
     activeTimeView: 'week',
+    currentDateTime,
 }
 
 // Reducer
@@ -29,11 +46,32 @@ export const utilsSlice = createSlice({
             }
             return state
         },
+        changeDateTimeData: (state, action) => {
+            const {
+                weekNumber,
+                month,
+                year,
+                firstDayOfTheWeek,
+                lastDayOfTheWeek,
+            } = action.payload
+            return {
+                ...state,
+                currentDateTime: {
+                    ...state.currentDateTime,
+                    weekNumber,
+                    month,
+                    year,
+                    firstDayOfTheWeek,
+                    lastDayOfTheWeek,
+                },
+            }
+        },
         default: (state) => {
             return state
         },
     },
 })
 
-export const { toggleMenu, changeActiveTimeView } = utilsSlice.actions
+export const { toggleMenu, changeActiveTimeView, changeDateTimeData } =
+    utilsSlice.actions
 export default utilsSlice.reducer
