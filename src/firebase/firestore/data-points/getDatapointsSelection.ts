a40b6af7cdd82f15bfa5a8a1e2fb3dce70809a61
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-15 11:38:30 +0100

    feat: getDatapointsSelection function
    First setup to get a selection of data points from firbase based on parameters

--- src/firebase/firestore/data-points/getDatapointsSelection.ts
+++ src/firebase/firestore/data-points/getDatapointsSelection.ts
@@ -1,1 +1,22 @@
-export default function getDatapointsSelection() {}
+import { collection, query, where, getDocs } from 'firebase/firestore'
+import { useAppSelector } from '../../../redux/reduxHooks'
+import { Metric } from '../../../types'
+import { db } from '../../firebase'
+
+export default async function getDatapointsSelection(
+    activeMetrics: Metric[],
+    userId: string,
+    currentYear: number
+) {
+    const dbQuery = query(
+        collection(db, 'data-points'),
+        where('userId', '==', userId),
+        where('year', '==', currentYear)
+    )
+
+    const querySnapshot = await getDocs(dbQuery)
+    querySnapshot.forEach((doc) => {
+        const { id } = doc
+        const { weekNumber, month, year, value } = doc.data()
+    })
+}
