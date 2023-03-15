28cae863420dda9d1a3d0ee91a37bddf1c80ebc4
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-15 23:38:03 +0100

    feat: Added Oura Resources
    Now Oura resources are also matched with db resources

--- src/services/matchResources.ts
+++ src/services/matchResources.ts
@@ -2,10 +2,10 @@ export default function matchServiceResourcesWithMetricNames(
     source: string,
     resource: string
 ) {
-    const shortenedResource = resource.split('-').slice(1, 2).join()
     let dbMetricName = ''
 
     if (source === 'fitbit') {
+        const shortenedResource = resource.split('-').slice(1, 2).join()
         switch (shortenedResource) {
             case 'activityCalories':
                 dbMetricName = 'total-average-calorie-burn'
@@ -21,6 +21,35 @@ export default function matchServiceResourcesWithMetricNames(
                 break
             default:
                 dbMetricName = ''
+        }
+    } else if (source === 'oura') {
+        switch (resource) {
+            case 'average_breath':
+                dbMetricName = 'respitory-rate'
+                break
+            case 'average_heart_rate':
+                dbMetricName = 'average-resting-heart-rate'
+                break
+            case 'average_hrv':
+                dbMetricName = 'average-resting-hrv'
+                break
+            case 'bedtime_end':
+                dbMetricName = 'average-wake-up'
+                break
+            case 'bedtime_start':
+                dbMetricName = 'average-time-to-bed'
+                break
+            case 'deep_sleep_duration':
+                dbMetricName = 'amount-of-deep-sleep'
+                break
+            case 'rem_sleep_duration':
+                dbMetricName = 'amount-of-rem-sleep'
+                break
+            case 'total_sleep_duration':
+                dbMetricName = 'amount-of-sleep'
+                break
+            default:
+                dbMetricName = 'unknown'
         }
     }
     return dbMetricName
