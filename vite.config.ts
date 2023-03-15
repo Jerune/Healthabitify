fdb78e957afd93b4ae5a63bddda15b80cff2e68f
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-15 15:30:03 +0100

    feat: Added proxy for Oura API request
    Fixes CORS issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/v2': 'https://api.ouraring.com'
+    }
+  },
   plugins: [react()],
 })
