e942f3e869468c4ea1f4d676750b5514af3e3ea4
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-21 22:10:58 +0100

    feat: Added date checks and multiple date ranges for getting api data
    Now checks if lastUpdated is more than 30 days ago, if yes it runs a batch of date ranges fetches per month instead of one big fetch

--- src/services/getApiData.ts
+++ src/services/getApiData.ts
@@ -1,7 +1,9 @@
+import calculateDifferenceWithToday from '../utils/compareDates'
 import {
     getDayBeforeAsString,
     getYesterdaysDateAsString,
 } from '../utils/getDatesAsString'
+import createMonthlyFetch from './createMonthlyFetch'
 import getSourceData from './getSourceData'
 
 export default async function getApiData(
@@ -13,35 +15,62 @@ export default async function getApiData(
         return 'error'
     }
 
+    // Dates
+    // dayBeforeLastUpdated to be used for Oura API as starts with day after start_date value
     const yesterdayString = getYesterdaysDateAsString()
-    // dayBeforeLastUpdated should be used to get Oura data as the API gathers
-    // data starting the day after the start_date value
     const dayBeforeLastUpdated = getDayBeforeAsString(lastUpdated)
-    const { baseUrl, resources } = getSourceData(source)
 
+    // API related Data
+    const { baseUrl, resources } = getSourceData(source)
     const headers = {
         headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`,
         },
     }
+    const endpointsList = []
 
-    const endpoints = resources.map((resource) => {
-        const url =
-            source === 'oura'
-                ? `/v2/usercollection/${resource}?start_date=${dayBeforeLastUpdated}&end_date=${yesterdayString}`
-                : `${baseUrl}/${resource}/date/${lastUpdated}/${yesterdayString}.json`
-        return {
-            url,
+    function getEndpoints({ start, end }) {
+        const endpoints = resources.map((resource) => {
+            const url =
+                source === 'oura'
+                    ? `/v2/usercollection/${resource}?start_date=${start}&end_date=${end}`
+                    : `${baseUrl}/${resource}/date/${start}/${end}.json`
+            return {
+                url,
+            }
+        })
+
+        endpointsList.push(...endpoints)
+    }
+
+    const differenceBetweenDatesInDays =
+        calculateDifferenceWithToday(lastUpdated)
+
+    // Verify if we need to break up API in monthly dates
+    if (differenceBetweenDatesInDays && differenceBetweenDatesInDays > 30) {
+        const fetchDates = createMonthlyFetch(lastUpdated, source)
+        fetchDates.forEach((fetchDate) => {
+            getEndpoints(fetchDate)
+        })
+    } else {
+        const dates = {
+            start: source === 'fitbit' ? lastUpdated : dayBeforeLastUpdated,
+            end: yesterdayString,
         }
-    })
+        getEndpoints(dates)
+    }
 
     const allData = await Promise.all(
-        endpoints.map(async (endpoint) => {
-            const response = await fetch(endpoint.url, headers)
-            const responseData = await response.json()
-
-            return responseData
+        endpointsList.map(async (endpoint) => {
+            try {
+                const response = await fetch(endpoint.url, headers)
+                const responseData = await response.json()
+                return responseData
+            } catch (error) {
+                console.log(error)
+                return error
+            }
         })
     )
 
