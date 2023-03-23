47431f99883abd423a4dddbf173574f49e46c541
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-23 16:38:29 +0100

    feat: Runs render on useEffect
    Now renders columns async when page loads on useEffect, also prepared rows already

--- src/pages/DataPage.tsx
+++ src/pages/DataPage.tsx
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDataGrid from '@inovua/reactdatagrid-community'
 import '@inovua/reactdatagrid-community/index.css'
 import { useParams } from 'react-router-dom'
 import MainContent from '../components/MainContent'
-import columnsMock from '../data/data-grid/columnsMock'
 import dataSourceMock from '../data/data-grid/dataSourceMock'
 import { useAppSelector } from '../redux/reduxHooks'
 import getActiveMetrics from '../features/DataGrid/getActiveMetrics'
+import buildColumns from '../features/DataGrid/buildColumns'
 
 function DataPage() {
     const { category } = useParams()
     const allMetrics = useAppSelector((state) => state.metrics)
-    if (category) {
-        const activeMetrics = getActiveMetrics(allMetrics, category)
-    }
     const title: string = category || 'Title'
+    const [activeColumns, setActiveColumns] = useState([])
+    const [activeRows, setActiveRows] = useState([])
+
+    useEffect(() => {
+        async function setDataGrid() {
+            if (category) {
+                const activeMetrics = getActiveMetrics(allMetrics, category)
+                const columns = await buildColumns(activeMetrics)
+                // const rowData = await buildRows(columns)
+                setActiveColumns(columns)
+                // setActiveRows(rowData)
+            }
+        }
+
+        setDataGrid()
+    }, [])
 
     return (
         <MainContent>
             <h1>{title.charAt(0).toUpperCase() + title.slice(1)}</h1>
             <ReactDataGrid
                 idProperty="id"
-                columns={columnsMock}
+                columns={activeColumns}
                 dataSource={dataSourceMock}
                 className="w-full h-full"
             />
