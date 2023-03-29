09b188e910cb6c1af6caca71af377725e5d0f9a2
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-03-29 17:17:09 +0200

    feat: Adjusted to fit different data sources

--- src/firebase/firestore/data-points/getDatapointsForPeriod.ts
+++ src/firebase/firestore/data-points/getDatapointsForPeriod.ts
@@ -1,5 +1,5 @@
 import { collection, query, where, getDocs } from 'firebase/firestore'
-import { DataPoint, Metric, Period } from '../../../types'
+import { Average, DataPoint, Metric, Period } from '../../../types'
 import kebabcaseToCamelcase from '../../../utils/kebabcaseToCamelcase'
 import { db } from '../../firebase'
 
@@ -10,38 +10,43 @@ export default async function getDatapointsForPeriod(
     const dateTitle = period.month
         ? `Y${period.year}-M${period.month}`
         : `Y${period.year}-W${period.week}`
-    const allDatapointsForPeriod = await Promise.all(
-        allMetrics.map(async (metric) => {
+
+    const metricsWithoutAutoSource = allMetrics.filter(
+        (metric) => metric.source !== 'auto'
+    )
+
+    const allDatapointsForPeriod: Average[] = await Promise.all(
+        metricsWithoutAutoSource.map(async (metric) => {
             const metricName = kebabcaseToCamelcase(metric.id)
             const datapoints: Partial<DataPoint>[] = []
-            const hasValidSource = metric.source !== 'auto'
-            if (hasValidSource) {
-                const metricRef = collection(db, `data-points-${metric.source}`)
-                const dbQuery = query(
-                    metricRef,
-                    where('metric', '==', metric.id),
-                    where('year', '==', period.year),
-                    period.month
-                        ? where('month', '==', period.month)
-                        : where('week', '==', period.week)
-                )
-                const querySnapshot = await getDocs(dbQuery)
-                querySnapshot.forEach((doc) => {
-                    const { value } = doc.data()
-                    datapoints.push({
-                        value,
-                    })
-                })
-            }
+            const collectionName = `data-points-${metric.source}`
+            const dbQuery = query(
+                collection(db, collectionName),
+                where('metric', '==', metric.id),
+                where('year', '==', period.year),
+                period.month
+                    ? where('month', '==', period.month)
+                    : where('weekNumber', '==', period.week)
+            )
+            const querySnapshot = await getDocs(dbQuery)
+            querySnapshot.forEach((doc) => {
+                const { value } = doc.data()
+                datapoints.push(value)
+            })
 
+            if (metric.frequency === 'weekly') {
+                const lastValue = datapoints.slice(-1)
+                return {
+                    [metricName]: lastValue,
+                }
+            }
             return {
                 [metricName]: datapoints,
             }
         })
     )
-
     return {
         period: dateTitle,
-        data: [...allDatapointsForPeriod],
+        data: allDatapointsForPeriod,
     }
 }
