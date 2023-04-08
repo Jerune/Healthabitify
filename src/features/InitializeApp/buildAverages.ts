7a46320d9a2bb684e4f15e3ef038d2ec71f9e3fc
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-04-08 21:44:50 +0200

    feat: buildAverages function creation
    function that builds up the structure for the store by getting all weekly, monthly and yearly averages from the firestore and building a readable structure from that data

---
+++ src/features/InitializeApp/buildAverages.ts
@@ -0,0 +1,65 @@
+/* eslint-disable no-await-in-loop */
+import getMonthlyYearlyAverages from '../../firebase/firestore/averages/getMonthlyYearlyAverages'
+import getWeeklyAverages from '../../firebase/firestore/averages/getWeeklyAverages'
+import { getDateTimeDateFromDateString } from '../../utils/getDateTimeData'
+
+async function buildAverages(lastUpdated: string) {
+    const availableDatesInAverages = {}
+
+    const lastUpdatedDate = getDateTimeDateFromDateString(lastUpdated)
+    const sameDayLastWeek = lastUpdatedDate.minus({ weeks: 1 })
+    const sameDayLastMonth = lastUpdatedDate.minus({ month: 1 })
+
+    let latestWeekNumber = sameDayLastWeek.weekNumber
+    let latestMonth = sameDayLastMonth.month
+    let currentYear = sameDayLastWeek.year
+
+    // Get all averages from months and weeks for a certain year
+    // To-Do enlarge to all years
+    while (currentYear > 2022) {
+        availableDatesInAverages[currentYear] = {
+            year: {},
+            months: {},
+            weeks: {},
+        }
+        while (latestMonth > 0) {
+            const average = await getMonthlyYearlyAverages(
+                currentYear,
+                latestMonth
+            )
+            if (average !== 'error') {
+                availableDatesInAverages[currentYear].months[latestMonth] =
+                    average
+                latestMonth -= 1
+            } else {
+                break
+            }
+        }
+        while (latestWeekNumber > 0) {
+            const average = await getWeeklyAverages(
+                currentYear,
+                latestWeekNumber
+            )
+            if (average !== 'error') {
+                availableDatesInAverages[currentYear].weeks[latestWeekNumber] =
+                    average
+                latestWeekNumber -= 1
+            } else {
+                break
+            }
+        }
+        const average = await getMonthlyYearlyAverages(currentYear)
+        if (average !== 'error') {
+            availableDatesInAverages[currentYear].year = average
+        } else {
+            break
+        }
+        currentYear -= 1
+        latestMonth = 12
+        latestWeekNumber = 52
+    }
+
+    return availableDatesInAverages
+}
+
+export default buildAverages
