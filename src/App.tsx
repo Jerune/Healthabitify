6e4ad731a0c5f02be3d9f5ce77bddd32c887c2f4
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-04-28 10:51:47 +0200

    feat: Moved AppStateInit
    AppstateInit is moved outside of Routes & LayoutRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,21 @@ import DataPage from './pages/DataPage'
 import NotFound from './pages/NotFound'
 import Settings from './pages/Settings'
 import Labs from './pages/Labs'
-import HeaderNav from './components/HeaderNav'
 import AppStateInit from './components/InitializeApp'
 import LayoutRoutes from './LayoutRoutes'
 
 function App() {
     return (
         <Router>
+            <AppStateInit />
             <Routes>
                 <Route path="/" element={<Login />} />
-                <Route path="callback" element={<OAuthPopup />} />
                 <Route element={<LayoutRoutes />}>
                     <Route path="dashboard" element={<Dashboard />} />
                     <Route path="data/:category" element={<DataPage />} />
                     <Route path="settings" element={<Settings />} />
                     <Route path="labs" element={<Labs />} />
+                    <Route path="callback" element={<OAuthPopup />} />
                     <Route path="*" element={<NotFound />} />
                 </Route>
             </Routes>
--- src/LayoutRoutes.tsx
+++ src/LayoutRoutes.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom'
 import HeaderNav from './components/HeaderNav'
-import AppStateInit from './components/InitializeApp'
 
 export default function LayoutRoutes() {
     return (
         <>
-            <AppStateInit />
             <HeaderNav />
             <Outlet />
         </>
