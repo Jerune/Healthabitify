425f2b38c4b9ab4428a8c22bd053fcbae9de6065
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-04-28 16:11:33 +0200

    style: Implemented Grid layout
    Solves positioning issue with cards

--- src/features/Dashboard/DashBoardContainer.tsx
+++ src/features/Dashboard/DashBoardContainer.tsx
@@ -2,9 +2,8 @@ import type { PropsWithChildren } from 'react'
 
 function DashBoardContainer({ children }: PropsWithChildren) {
     return (
-        <div className="flex flex-row flex-wrap py-8 px-20 justify-between gap-16 w-full ">
+        <div className="grid justify-center justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-8 px-10 lg:px-20 gap-8 w-full ">
             {children}
-            <div className="grow"> </div>
         </div>
     )
 }
