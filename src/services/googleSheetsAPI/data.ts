ee775b51854856134b7412a4d0e795b0ebd84ed5
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-05-02 16:43:27 +0200

    Remove Google keys

--- src/services/googleSheetsAPI/data.ts
+++
@@ -1,6 +0,0 @@
-const APIKey = 'AIzaSyCA804z8krvmsG9lU6jo_uE-J_yDZugdBg'
-const clientId =
-    '508872663265-irct9hid1o4dldbulsg2r0tt8lg7obaq.apps.googleusercontent.com'
-const clientSecret = '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'
-const accessToken =
-    'ya29.a0AWY7CkmMhzPCJNO14TLs2PQtVqONgQAHS7uaK_U5IxDQEOi9nDOXsLCudNQnztrE1YbsF_i4OK_7FwXOyYEnZRtuPFYi0nsKwtdW8N-Dv0zgMeSy1S-9qhRGEDAVXJUje8mw0kiLMrZyP6lFSgwcmSAreyV1aCgYKARcSARESFQG1tDrp2pc7ThdV_XzyrxVKgqcXeQ0163'
