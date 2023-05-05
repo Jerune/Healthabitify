46dbc6e39cabd7943f3b8c09cc0316b27efb0a2c
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-05-05 12:16:00 +0200

    refactor: Adjusted data for reuse in case needed later
    Available for upcoming weeks if needed

--- src/services/googleSheetsAPI/getSheetData.ts
+++ src/services/googleSheetsAPI/getSheetData.ts
@@ -4,10 +4,11 @@ import { DataPoint } from '../../types'
 import addDatapoints from '../../firebase/firestore/data-points/addDatapoints'
 
 async function getSheetData() {
-    const auth =
-        'Bearer ya29.a0AWY7CklFoh4r7NFNyrvGA4B5dfyt52AH5BK3_g5mOmCcJKGsx6p4h48l_UTtNhEWLRIOdvezJShtoCzalDlsyMHxvCjpGq1O4hUoGuvrquSLFlBPwCZ92AJXovBQv1ACy5wefHTkX8U75Ckh6yAiRWuSiOcTeAaCgYKAcwSARESFQG1tDrpE9zcMw0Hqgsf2aGHbva8PQ0165'
+    const auth = 'Bearer'
+    const sheetId = '1ehnbMFvNaCU46KZ_WM0V9Kan9i4WoDbP8ux6CXBQVtI'
+    const tabIdRange = 'Weekly!A1:R12'
     const responseValues = await fetch(
-        'https://sheets.googleapis.com/v4/spreadsheets/1-WPEgpiI_5eiOWc3hoV96_vjt-LV2d0dRHWvXH4-3UA/values/Weekly!A1:BA9?key=AIzaSyCA804z8krvmsG9lU6jo_uE-J_yDZugdBg',
+        `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${tabIdRange}?key=AIzaSyCA804z8krvmsG9lU6jo_uE-J_yDZugdBg`,
         {
             headers: {
                 'Content-Type': 'application/json',
@@ -54,7 +55,7 @@ async function getSheetData() {
         // if (datapoints.length > 0) {
         //     addDatapoints(datapoints)
         // }
-        console.log(datapoints)
+        // console.log(datapoints)
         datapoints = []
     })
 }
