bc120eac40d100476287f5c7084330fbbf2edada
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-05-17 17:07:04 +0200

    feat: Adapted to all value inputs
    Now supports all value inputs so it can be used more universally throughout the app

--- src/features/DataOutputManagement/adjustValueOutput.ts
+++ src/features/DataOutputManagement/adjustValueOutput.ts
@@ -1,6 +1,7 @@
 import convertMillisecondsToTime from '../../utils/convertMillisecondsToTime'
 import { Metric } from '../../types'
 import addDecimals from './addDecimals'
+import forceNumberReturn from '../../utils/forceNumberReturn'
 
 export default function adjustValueOutput(
     metric: Metric,
@@ -20,13 +21,17 @@ export default function adjustValueOutput(
 
     // Change to hours or minutes format as string in case of time or duration
     if (metric.dataType === 'time' || metric.dataType === 'duration') {
-        const amountOfMinutes = value * 60
+        const amountOfMinutes = forceNumberReturn(value) * 60
         const hours = Math.floor(amountOfMinutes / 60)
         const minutes = Math.round(amountOfMinutes - hours * 60)
         const hoursAsString = String(hours).padStart(2, '0')
         const minutesAsString = String(minutes).padStart(2, '0')
 
         return `${hoursAsString}:${minutesAsString}`
+    }
+
+    if (metric === null) {
+        return 'null'
     }
 
     // Adapt to output & Change to String without any adjustments
