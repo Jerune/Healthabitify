3062802f8f840c0101f3e8db017701672f70ff62
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-05-25 11:46:19 +0200

    refactor: Simplified code
    Now uses the startOf and endOf functions from Luxon to calculate start and end date of the week

--- src/features/TimesDatesModule/getWeekDays.ts
+++ src/features/TimesDatesModule/getWeekDays.ts
@@ -1,15 +1,8 @@
 import { DateTime } from 'luxon'
 
-function getWeekDays(currentDate: DateTime) {
-    let lastDayOfTheWeek = currentDate
-
-    for (let i = 1; i < 7; i += 1) {
-        const newDate = currentDate.plus({ days: i })
-        if (newDate.weekNumber === currentDate.weekNumber) {
-            lastDayOfTheWeek = newDate
-        }
-    }
-    const firstDayOfTheWeek = lastDayOfTheWeek.minus({ days: 7 })
+async function getWeekDays(currentDate: DateTime) {
+    const firstDayOfTheWeek = currentDate.startOf('week')
+    const lastDayOfTheWeek = currentDate.endOf('week')
 
     return {
         firstDayOfTheWeek,
