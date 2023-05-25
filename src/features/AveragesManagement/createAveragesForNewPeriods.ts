86ca6ad1eb15589b34a3df924e5e8de4b6ddb00a
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-05-25 11:44:58 +0200

    feat: createAveragesForNewPeriods function
    Separate functions to get averages for new periods

---
+++ src/features/AveragesManagement/createAveragesForNewPeriods.ts
@@ -0,0 +1,19 @@
+import addAverages from '../../firebase/firestore/averages/addAverages'
+import getDatapointsForPeriod from '../../firebase/firestore/data-points/getDatapointsForPeriod'
+import { Metric, Period } from '../../types'
+import calculateAveragesForPeriod from './calculateAveragesForPeriod'
+
+async function createAveragesForNewPeriods(
+    newPeriods: Period[],
+    allMetrics: Metric[]
+) {
+    newPeriods.map(async (newPeriod) => {
+        const datapoints = await getDatapointsForPeriod(allMetrics, newPeriod)
+        if (datapoints.data.length > 0) {
+            const averages = await calculateAveragesForPeriod(datapoints)
+            addAverages(averages)
+        }
+    })
+}
+
+export default createAveragesForNewPeriods
