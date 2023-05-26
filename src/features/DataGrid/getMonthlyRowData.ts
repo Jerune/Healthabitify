37bf2f6ee1807fe0c17bcc68ded2ab03ab1fdc0d
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-05-26 22:13:46 +0200

    feat: getMonthlyRowData now features all months arranged
    Added support for inter-yearly comparisons as well as sorting and conditional coloring

--- src/features/DataGrid/getMonthlyRowData.ts
+++ src/features/DataGrid/getMonthlyRowData.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
 import kebabcaseToCamelcase from '../../utils/kebabcaseToCamelcase'
 import adjustValueOutput from '../DataOutputManagement/adjustValueOutput'
+import metricsWithZeroValues from '../../data/data-grid/metricsWithZeroValues'
 
 function getMonthlyRowData(activeMetrics, allAverages) {
     const rows = []
@@ -10,35 +11,65 @@ function getMonthlyRowData(activeMetrics, allAverages) {
         const activeYear = Number(year.split('Y')[1])
         const months = Object.keys(allAverages[year].months)
         months.forEach((month) => {
-            const monthNumber = month.split('M')[1].padStart(2, '0')
+            const monthNumber = Number(month.split('M')[1])
             const monthName = DateTime.fromISO(
-                `${activeYear}-${monthNumber}-01`
+                `${activeYear}-${String(monthNumber).padStart(2, '0')}-01`
             ).toFormat('MMMM')
             const dateTitle = `${monthName} ${activeYear}`
 
             // Setting default row data
             const row = {
-                id: monthNumber,
+                year: activeYear,
+                month: monthNumber,
+                id: `${activeYear}-${monthNumber}`,
                 date: dateTitle,
             }
             // Retrieving average data from every metric for that month
             activeMetrics.forEach((metric) => {
+                let yearString = year
                 const metricId = kebabcaseToCamelcase(metric.id)
                 const metricAverageValueThismonth =
-                    allAverages[year].months[month][metricId]
+                    allAverages[yearString].months[month][metricId]
                 row[metricId] = adjustValueOutput(
                     metric,
                     metricAverageValueThismonth
                 )
-                if (months.includes(`M${monthNumber - 1}`)) {
-                    const metricAverageValueLastmonth =
-                        allAverages[year].months[`M${monthNumber - 1}`][
+                // Verify if previousMonth is December and
+                // changing year and month if this is the case
+                let previousMonth = monthNumber - 1
+                let metricAverageValuePreviousPeriod = 0
+                let availableMonths = months
+                if (previousMonth === 0) {
+                    yearString = `Y${activeYear - 1}`
+                    // Verify if previous year is available
+                    if (allAverages[yearString]) {
+                        // Gathering months from prvious year
+                        availableMonths = Object.keys(
+                            allAverages[yearString].months
+                        )
+                        previousMonth = 12
+                    }
+                }
+                // Look for last real value to compare with
+                while (availableMonths.includes(`M${previousMonth}`)) {
+                    metricAverageValuePreviousPeriod =
+                        allAverages[yearString].months[`M${previousMonth}`][
                             metricId
                         ]
-                    row[`prev${metricId}`] = adjustValueOutput(
-                        metric,
-                        metricAverageValueLastmonth
-                    )
+                    // Passes 0 values if for a metric where 0 values should be ignored
+                    if (
+                        metricAverageValuePreviousPeriod === 0 &&
+                        !metricsWithZeroValues.includes(metric.id)
+                    ) {
+                        previousMonth -= 1
+                        // Writes prev comparison data whenever last real value is found
+                    } else {
+                        row[`prev${metricId}`] = adjustValueOutput(
+                            metric,
+                            metricAverageValuePreviousPeriod
+                        )
+                        break
+                    }
                 }
             })
 
@@ -46,7 +77,13 @@ function getMonthlyRowData(activeMetrics, allAverages) {
         })
     })
 
-    return rows
+    const sortedRows = rows.sort((a, b) => {
+        if (a.year !== b.year) {
+            return b.year - a.year // Sort by year in descending order
+        }
+        return b.month - a.month // Sort by month in descending order within the same year
+    })
+    return sortedRows
 }
 
 export default getMonthlyRowData
