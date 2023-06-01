aa4bf4153ce0202f5604c079bee3aa998f2e3ec9
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-06-01 16:07:56 +0200

    feat: Manual Imports on Pages when logedIn
    The manual edits modals get automatically loaded through LayoutRoutes

--- src/LayoutRoutes.tsx
+++ src/LayoutRoutes.tsx
@@ -1,13 +1,19 @@
 import { Outlet } from 'react-router-dom'
 import HeaderNav from './components/HeaderNav'
 import AppStateInit from './components/InitializeApp'
-import Loading from './components/Loading'
+import { useAppSelector } from './redux/reduxHooks'
+import ManualDataGrid from './features/ManualDataGrid/ManualDataGrid'
 
 export default function LayoutRoutes() {
+    const manualDataGridOpen = useAppSelector(
+        (state) => state.utils.manualDataGridOpen
+    )
+
     return (
         <>
             <AppStateInit />
             <HeaderNav />
+            {manualDataGridOpen && <ManualDataGrid />}
             <Outlet />
         </>
     )
