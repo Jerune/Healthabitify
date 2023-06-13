ebb7d960dc74f88c5ec456dd9c1b51c31a243b35
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-06-13 20:31:05 +0200

    feat: buildManualRows function
    Takes the daily datapoints and creates a grid from those based on the active days of the week

--- src/features/DataGrid/buildManualRows.ts
+++ src/features/DataGrid/buildManualRows.ts
@@ -1,20 +1,40 @@
 function buildManualRows(datapoints, dates: string[]) {
     const rows = []
 
+    datapoints.forEach((metricObject, index: number) => {
+        const keys = Object.keys(metricObject)
+        const metricId = keys[0]
+        const weeklyData = metricObject[metricId]
 
-    datapoints.forEach((metric) => {
+        const metricName = keys[0].split(/(?=[A-Z])/).join(' ')
+        const { type } = metricObject
+
         const row = {
-            metric,
+            metric: metricName,
+            id: index.toString(),
+            cells: {},
         }
 
-        metric.forEach((dataObject) => {
-            const {date} = dataObject
+        dates.forEach((date) => {
+            for (let i = 0; i < weeklyData.length; i += 1) {
+                if (weeklyData[i].date === date) {
+                    const cellId = weeklyData[i].id
+                    const cell = {
+                        id: cellId,
+                        date,
+                        value: weeklyData[i].value,
+                    }
+                    row[date] = cell.value
+                    row.cells[date] = cellId
+                }
+            }
+        })
 
-            const {id, value} = dataObject
-
-            row[id] = 
-        } )
+        rows.push(row)
     })
+
+    const sortedRows = rows.sort((a, b) => a.id - b.id)
+    return sortedRows
 }
 
 export default buildManualRows
