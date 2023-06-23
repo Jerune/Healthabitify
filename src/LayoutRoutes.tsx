e70643da0afc61f65518f1dfdec262aab8fb60fd
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-06-23 22:14:38 +0200

    feat: Add updateMessage component to all LayoutRoutes

--- src/LayoutRoutes.tsx
+++ src/LayoutRoutes.tsx
@@ -3,17 +3,17 @@ import HeaderNav from './components/HeaderNav'
 import AppStateInit from './components/InitializeApp'
 import { useAppSelector } from './redux/reduxHooks'
 import ManualDataGridContainer from './features/ManualDataGrid/ManualDataGridContainer'
+import UpdateMessage from './components/UpdateMessage'
 
 export default function LayoutRoutes() {
-    const manualDataGridOpen = useAppSelector(
-        (state) => state.utils.manualDataGridOpen
-    )
+    const utils = useAppSelector((state) => state.utils)
 
     return (
         <>
             <AppStateInit />
+            <UpdateMessage />
             <HeaderNav />
-            {manualDataGridOpen && <ManualDataGridContainer />}
+            {utils.manualDataGridOpen && <ManualDataGridContainer />}
             <Outlet />
         </>
     )
