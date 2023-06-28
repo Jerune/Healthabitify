9ea1e3a804335df972eb8ef9eccc592d1a42ba6f
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-06-28 22:03:16 +0200

    refactor: Removed Google API from wearables list

--- src/features/SettingsMenu/Wearables.tsx
+++ src/features/SettingsMenu/Wearables.tsx
@@ -13,11 +13,11 @@ function Wearables() {
                 id="238Z6R"
                 scope="activity cardio_fitness heartrate electrocardiogram oxygen_saturation respiratory_rate sleep temperature weight settings"
             />
-            <AuthorizeWearableButton
+            {/* <AuthorizeWearableButton
                 url="https://accounts.google.com/o/oauth2/auth"
                 id="508872663265-irct9hid1o4dldbulsg2r0tt8lg7obaq.apps.googleusercontent.com"
                 scope="https://www.googleapis.com/auth/spreadsheets.readonly"
-            />
+            /> */}
         </>
     )
 }
