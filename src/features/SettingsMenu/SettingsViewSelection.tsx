5a3c28b9f5e2e5376488a63c69a12873fa6b1e85
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-06-28 22:02:48 +0200

    feat: New SettingsViewSelection modulw
    Now users can choose between wearables and metrics section in the settingsmenu

---
+++ src/features/SettingsMenu/SettingsViewSelection.tsx
@@ -0,0 +1,24 @@
+function SettingsViewSelection(props) {
+    const { setDetailView } = props
+
+    return (
+        <div className="w-full flex justify-center flex-row items-center gap-12">
+            <button
+                type="button"
+                className="h-24 w-52 rounded-lg border-solid border-2 text-lg bg-white hover:bg-green-400 hover:italic hover:font-bold hover:border-0 hover:text-xl hover:transition-colors"
+                onClick={() => setDetailView('metrics')}
+            >
+                Metrics
+            </button>
+            <button
+                type="button"
+                className="h-24 w-52 rounded-lg border-solid border-2 text-lg bg-white hover:bg-green-400 hover:italic hover:font-bold hover:border-0 hover:text-xl hover:transition-colors"
+                onClick={() => setDetailView('wearables')}
+            >
+                Wearables
+            </button>
+        </div>
+    )
+}
+
+export default SettingsViewSelection
