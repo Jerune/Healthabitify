56d1358d49f97f8a6aa4aec78b36966467ca7aa7
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-06-28 22:00:52 +0200

    feat: wearablesCategories data file
    categories list for werables section of the settingsmenu

---
+++ src/data/wearablesCategories.ts
@@ -0,0 +1,19 @@
+const wearablesCategories = [
+    {
+        id: 'oura',
+        name: 'Oura',
+        iconName: 'FaRing',
+    },
+    {
+        id: 'fitbit',
+        name: 'Fitbit',
+        iconName: 'SiFitbit',
+    },
+    {
+        id: 'bodypedia',
+        name: 'BodyPedia',
+        iconName: 'IoScaleSharp',
+    },
+]
+
+export default wearablesCategories
