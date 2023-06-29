eaf968675920c068f534705a21d38c2f5713b81a
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-06-29 23:26:21 +0200

    refactor: Split up in multiple elements
    Now exports separate elements for all wearables

--- src/features/SettingsMenu/Wearables.tsx
+++ src/features/SettingsMenu/Wearables.tsx
@@ -1,25 +1,33 @@
 import AuthorizeWearableButton from '../../services/AuthorizeWearableButton'
 
-function Wearables() {
+function OuraButton() {
     return (
-        <>
-            <AuthorizeWearableButton
-                url="https://cloud.ouraring.com/oauth/authorize"
-                id="OBGZFELJOIDPH67I"
-                scope="daily heartrate workout session"
-            />
-            <AuthorizeWearableButton
-                url="https://www.fitbit.com/oauth2/authorize"
-                id="238Z6R"
-                scope="activity cardio_fitness heartrate electrocardiogram oxygen_saturation respiratory_rate sleep temperature weight settings"
-            />
-            {/* <AuthorizeWearableButton
-                url="https://accounts.google.com/o/oauth2/auth"
-                id="508872663265-irct9hid1o4dldbulsg2r0tt8lg7obaq.apps.googleusercontent.com"
-                scope="https://www.googleapis.com/auth/spreadsheets.readonly"
-            /> */}
-        </>
+        <AuthorizeWearableButton
+            url="https://cloud.ouraring.com/oauth/authorize"
+            id="OBGZFELJOIDPH67I"
+            scope="daily heartrate workout session"
+        />
     )
 }
 
-export default Wearables
+function FitbitButton() {
+    return (
+        <AuthorizeWearableButton
+            url="https://www.fitbit.com/oauth2/authorize"
+            id="238Z6R"
+            scope="activity cardio_fitness heartrate electrocardiogram oxygen_saturation respiratory_rate sleep temperature weight settings"
+        />
+    )
+}
+
+function GoogleSheetsButton() {
+    return (
+        <AuthorizeWearableButton
+            url="https://accounts.google.com/o/oauth2/auth"
+            id="508872663265-irct9hid1o4dldbulsg2r0tt8lg7obaq.apps.googleusercontent.com"
+            scope="https://www.googleapis.com/auth/spreadsheets.readonly"
+        />
+    )
+}
+
+export { OuraButton, FitbitButton, GoogleSheetsButton }
