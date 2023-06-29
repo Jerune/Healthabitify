bd1fc33849bba23559c934c5da13f27c0dd6bf17
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-06-29 23:25:02 +0200

    style: Style changes hover
    New color

--- src/features/SettingsMenu/SettingsViewSelection.tsx
+++ src/features/SettingsMenu/SettingsViewSelection.tsx
@@ -5,14 +5,14 @@ function SettingsViewSelection(props) {
         <div className="w-full flex justify-center flex-row items-center gap-12">
             <button
                 type="button"
-                className="h-24 w-52 rounded-lg border-solid border-2 text-lg bg-white hover:bg-green-400 hover:italic hover:font-bold hover:border-0 hover:text-xl hover:transition-colors"
+                className="h-24 w-52 rounded-lg border-solid border-2 text-lg bg-white hover:bg-palette-300 hover:italic hover:font-bold hover:border-0 hover:text-xl hover:transition-colors"
                 onClick={() => setDetailView('metrics')}
             >
                 Metrics
             </button>
             <button
                 type="button"
-                className="h-24 w-52 rounded-lg border-solid border-2 text-lg bg-white hover:bg-green-400 hover:italic hover:font-bold hover:border-0 hover:text-xl hover:transition-colors"
+                className="h-24 w-52 rounded-lg border-solid border-2 text-lg bg-white hover:bg-palette-300 hover:italic hover:font-bold hover:border-0 hover:text-xl hover:transition-colors"
                 onClick={() => setDetailView('wearables')}
             >
                 Wearables
