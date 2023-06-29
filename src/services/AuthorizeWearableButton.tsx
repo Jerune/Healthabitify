84be0e69abb12bf95688a4d2be8d5387fa0fbea7
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-06-29 23:29:40 +0200

    style: Different style & functionalities
    Minor style changes as well as some action changes adding updateMessage whenever getAuth() is successfull or throws an error

--- src/services/AuthorizeWearableButton.tsx
+++ src/services/AuthorizeWearableButton.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable no-console */
 import { useOAuth2 } from '@tasoskakour/react-use-oauth2'
+import { useState } from 'react'
+import { useAppDispatch } from '../redux/reduxHooks'
+import { setUpdateMessage } from '../redux/reducers/utilsReducer'
 
 type AuthorizeApi = {
     url: string
@@ -8,39 +11,40 @@ type AuthorizeApi = {
 }
 
 function AuthorizeWearableButton({ url, id, scope }: AuthorizeApi) {
-    const { data, loading, error, getAuth } = useOAuth2({
+    const [newTokenActive, setNewTokenActive] = useState(false)
+    const dispatch = useAppDispatch()
+    let textContent = 'Renew Authorization'
+    let newToken = ''
+    const { loading, getAuth } = useOAuth2({
         authorizeUrl: url,
         clientId: id,
         redirectUri: `${document.location.origin}/callback`,
         responseType: 'token',
         scope,
-        onSuccess: (payload) => console.log('Success', payload),
-        onError: (error_) => console.log('Error', error_),
+        onSuccess: (payload) => {
+            newToken = payload.access_token
+            setNewTokenActive(true)
+        },
+        onError: (error_) => {
+            dispatch(setUpdateMessage(`Error: ${error_}`))
+        },
     })
-
-    const isLoggedIn = Boolean(data?.access_token)
-    let textContent = 'Login'
-
-    if (error) {
-        textContent = 'An error has produced'
-    }
 
     if (loading) {
         textContent = 'Loading...'
     }
 
-    if (isLoggedIn) {
-        textContent = JSON.stringify(data?.access_token)
-    }
-
     return (
-        <button
-            className="rounded-lg px-12 py-8 bg-green-600 text-white"
-            type="button"
-            onClick={() => getAuth()}
-        >
-            {textContent}
-        </button>
+        <>
+            <button
+                className="px-8 py-4 bg-palette-300 text-black"
+                type="button"
+                onClick={() => getAuth()}
+            >
+                {textContent}
+            </button>
+            {newTokenActive && <p>{`Copy to token field: ${newToken}`}</p>}
+        </>
     )
 }
 
