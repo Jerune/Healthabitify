b83bc4699b7d2bbc141e3000d0d2c457a7193d5f
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-06-29 23:23:23 +0200

    style: Padding resizing
    slightly larger x-axe padding added

--- src/features/SettingsMenu/SettingsContentField.tsx
+++ src/features/SettingsMenu/SettingsContentField.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react'
 
 function SettingsContentField({ children }: PropsWithChildren) {
     return (
-        <div className="flex flex-col justify-start items-start h-vh p-6 gap-6 grow">
+        <div className="flex flex-col justify-start items-start h-vh py-6 px-10 gap-6 grow">
             {children}
         </div>
     )
