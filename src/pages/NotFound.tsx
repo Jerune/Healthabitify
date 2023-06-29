71cc16e1ff2ed5fc9c4159bb6fa370a7301c4407
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-06-29 23:27:09 +0200

    feat: 404 Page
    Now features some general title and info on 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,7 +3,13 @@ import MainContent from '../components/MainContent'
 function NotFound() {
     return (
         <MainContent>
-            <h1>404</h1>
+            <div className="flex flex-col pl-[20%] w-full justify-start items-start">
+                <h1 className="italic font-bold">Not Found</h1>
+                <p className="text-base mt-4">
+                    The URL you have entered does not exists. Please open the
+                    main menu and select an option from the list.
+                </p>
+            </div>
         </MainContent>
     )
 }
