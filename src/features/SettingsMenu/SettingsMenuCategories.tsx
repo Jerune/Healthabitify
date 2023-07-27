c5b0ded7d8ad6c4d1cc02cca6493216976a73873
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-07-27 21:44:11 +0200

    feat: useState for hiding initial metrics menu on mobile
    Now initial metrics selection menu hides on mobile and stays on desktop

--- src/features/SettingsMenu/SettingsMenuCategories.tsx
+++ src/features/SettingsMenu/SettingsMenuCategories.tsx
@@ -15,7 +15,14 @@ import wearablesCategories from '../../data/wearablesCategories'
 import categoriesList from '../../data/categories'
 
 function SettingsMenuCategories(props) {
-    const { detailView, setMetrics, setWearables, activeCategory } = props
+    const {
+        detailView,
+        setMetrics,
+        setWearables,
+        activeCategory,
+        hideMenuCategories,
+        setHideMenuCategories,
+    } = props
 
     const Icons = {
         oura: <FaRing />,
@@ -31,24 +38,30 @@ function SettingsMenuCategories(props) {
         lifestyle: <RiGobletFill />,
     }
 
+    const hideMenuOnMobile = hideMenuCategories ? 'hidden' : 'flex flex-col'
+
     const dataSource =
         detailView === 'wearables' ? wearablesCategories : categoriesList
     const categories = dataSource.map((category) => {
         const Icon = Icons[category.id]
         return (
             <button
-                className={`w-72 flex flex-row gap-2 justify-start items-center text-xl py-7 pl-8 rounded-lg ${
+                className={`w-full md:w-72 lg:w-96 flex flex-row gap-2 justify-start items-center text-xl pt-8 pb-7 px-8 rounded-lg shadow-lg ${
                     activeCategory.id === category.id
                         ? 'bg-palette-600 italic text-white'
                         : 'bg-white'
                 } hover:bg-palette-600 hover:text-2xl hover:italic hover:text-white hover:transition-colors hover:underline`}
                 type="button"
                 key={category.name}
-                onClick={() =>
-                    detailView === 'metrics'
-                        ? setMetrics(category.id)
-                        : setWearables(category.id)
-                }
+                onClick={() => {
+                    if (detailView === 'metrics') {
+                        setMetrics(category.id)
+                        setHideMenuCategories(true)
+                    } else {
+                        setWearables(category.id)
+                        setHideMenuCategories(true)
+                    }
+                }}
             >
                 <i>{Icon}</i>
                 <h2 className="text-2xl font-normal">{category.name}</h2>
@@ -56,7 +69,13 @@ function SettingsMenuCategories(props) {
         )
     })
 
-    return <div className="flex flex-col gap-4">{categories}</div>
+    return (
+        <div
+            className={`md:flex md:flex-col gap-4 w-screen md:w-full ${hideMenuOnMobile}`}
+        >
+            {categories}
+        </div>
+    )
 }
 
 export default SettingsMenuCategories
