b5f72fa9bc097b38f02831e0c163208a363c1b30
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-07-27 21:40:03 +0200

    style: padding main content

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react'
 
 function MainContent({ children }: PropsWithChildren) {
     return (
-        <main className="flex flex-col justify-center items-start grow w-full pb-6">
+        <main className="flex flex-col justify-center items-start grow w-full pb-8">
             {children}
         </main>
     )
