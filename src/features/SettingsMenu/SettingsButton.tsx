836684668ce28afb78b8337942b8b0c2683ad07d
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-07-27 21:42:59 +0200

    style: Responsive Settings
    Now made responsive for both mobile and desktop integration

--- src/features/SettingsMenu/SettingsButton.tsx
+++ src/features/SettingsMenu/SettingsButton.tsx
@@ -6,7 +6,7 @@ function SettingsButton({
     text,
     onClick,
 }: SettingsButtonType): JSX.Element {
-    const genericStyles = 'w-[50%] px-5 py-3 text-base'
+    const genericStyles = 'w-[50%] max-w-[600px] px-5 py-3 text-base'
     const activeStyles =
         'bg-green-600 hover:opacity-90 text-white pointer cursor-pointer'
     const nonActiveStyles =
--- src/features/SettingsMenu/SettingsViewSelection.tsx
+++ src/features/SettingsMenu/SettingsViewSelection.tsx
@@ -2,17 +2,17 @@ function SettingsViewSelection(props) {
     const { setDetailView } = props
 
     return (
-        <div className="w-full flex justify-center flex-row items-center gap-12">
+        <div className="w-full flex flex-col justify-center md:flex-row items-center gap-12">
             <button
                 type="button"
-                className="h-24 w-52 rounded-lg border-solid border-2 text-lg bg-white hover:bg-palette-300 hover:italic hover:font-bold hover:border-0 hover:text-xl hover:transition-colors"
+                className="h-24 w-52 rounded-lg border-solid border-2 text-lg bg-white hover:bg-palette-600 hover:italic hover:font-bold hover:border-0 hover:text-2xl hover:transition-colors hover:text-white hover:underline hover:underline-offset-4"
                 onClick={() => setDetailView('metrics')}
             >
                 Metrics
             </button>
             <button
                 type="button"
-                className="h-24 w-52 rounded-lg border-solid border-2 text-lg bg-white hover:bg-palette-300 hover:italic hover:font-bold hover:border-0 hover:text-xl hover:transition-colors"
+                className="h-24 w-52 rounded-lg border-solid border-2 text-lg bg-white hover:bg-palette-600 hover:italic hover:font-bold hover:border-0 hover:text-xl hover:transition-colors hover:text-white hover:underline hover:underline-offset-4"
                 onClick={() => setDetailView('wearables')}
             >
                 Wearables
