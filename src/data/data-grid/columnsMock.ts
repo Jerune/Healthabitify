57d9484fc817e269e7c2dea01cfab43d4c590577
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-08-21 13:34:11 +0200

    fix: Added Types
    Additional types for columns from Data Grid

--- src/data/data-grid/columnsMock.ts
+++ src/data/data-grid/columnsMock.ts
@@ -1,38 +1,62 @@
+import { CellProps } from '@inovua/reactdatagrid-community/types'
+
 /* eslint-disable no-param-reassign */
 const columnsMock = [
     { name: 'date', header: 'Date' },
     {
         name: 'RHR',
         header: 'Average Resting Heart Rate (bpm)',
-        onRender: (cellProps, { data }) => {
+        onRender: (
+            cellProps: CellProps,
+            { data }: { data: Record<string, number | string> }
+        ) => {
+            cellProps.style = cellProps.style || {}
+            const style = cellProps.style as React.CSSProperties
+
             if (data.RHR < data.prevRHR) {
-                cellProps.style.background = 'green'
+                style.background = 'green'
             } else if (data.RHR > data.prevRHR) {
-                cellProps.style.background = 'red'
+                style.background = 'red'
             } else {
-                cellProps.style.background = 'orange'
+                style.background = 'orange'
             }
         },
     },
     {
         name: 'HRV',
         header: 'Average Resting Heart Rate Variability (ms)',
-        onRender: (cellProps, { data }) => {
-            cellProps.style.background = data.HRV > 50 ? 'green' : 'red'
+        onRender: (
+            cellProps: CellProps,
+            { data }: { data: Record<string, number | string> }
+        ) => {
+            cellProps.style = cellProps.style || {}
+            const style = cellProps.style as React.CSSProperties
+            style.background = Number(data.HRV) > 50 ? 'green' : 'red'
         },
     },
     {
         name: 'Blood',
         header: 'Blood Oxygen (SpO2) (%)',
-        onRender: (cellProps, { data }) => {
-            cellProps.style.background = data.Blood > '96%' ? 'green' : 'red'
+        onRender: (
+            cellProps: CellProps,
+            { data }: { data: Record<string, number | string> }
+        ) => {
+            cellProps.style = cellProps.style || {}
+            const style = cellProps.style as React.CSSProperties
+
+            style.background = data.Blood > '96%' ? 'green' : 'red'
         },
     },
     {
         name: 'Resp',
         header: 'Respitory Rate (x/min)',
-        onRender: (cellProps, { data }) => {
-            cellProps.style.background = data.Resp < 14.3 ? 'green' : 'red'
+        onRender: (
+            cellProps: CellProps,
+            { data }: { data: Record<string, number | string> }
+        ) => {
+            cellProps.style = cellProps.style || {}
+            const style = cellProps.style as React.CSSProperties
+            style.background = Number(data.Resp) < 14.3 ? 'green' : 'red'
         },
     },
 ]
