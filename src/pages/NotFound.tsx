e059fe82fc957f36ff29ce64099fc8bd8157a630
Jeroen van de Sanden <vandesanden@hotmail.com>
2023-09-11 10:32:57 +0200

    refactor: Added HeaderNav to page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,20 @@
+import HeaderNav from '../components/HeaderNav'
 import MainContent from '../components/MainContent'
 
 function NotFound() {
     return (
-        <MainContent>
-            <div className="flex flex-col pl-[20%] w-full justify-start items-start">
-                <h1 className="italic font-bold">Not Found</h1>
-                <p className="text-base mt-4">
-                    The URL you have entered does not exists. Please open the
-                    main menu and select an option from the list.
-                </p>
-            </div>
-        </MainContent>
+        <>
+            <HeaderNav />
+            <MainContent>
+                <div className="flex flex-col pl-[20%] w-full justify-start items-start">
+                    <h1 className="italic font-bold">Not Found</h1>
+                    <p className="text-base mt-4">
+                        The URL you have entered does not exists. Please open
+                        the main menu and select an option from the list.
+                    </p>
+                </div>
+            </MainContent>
+        </>
     )
 }
 
